c62e31e9f3056e2a0e02ddb4b722379e0bf07d08
Sheridan Melton <99241310+XxElijahMxX@users.noreply.github.com>
2023-02-01 10:00:57 -0500

    add overlay screen to display hamburger menu options on mobile devices and added css media queries, navbar done

--- cinna-sweets/src/Components/Navbar/Navbar.jsx
+++ cinna-sweets/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
-import { MdOutlineRestaurantMenu, MDOutlineRestaurantMenu } from 'react-icons/md'
+import { MdOutlineRestaurantMenu } from 'react-icons/md'
 import { images } from '../../constants'
 import './Navbar.css'
 
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
   return (
 <nav className='app__navbar'>
   <div className='app__navbar-logo'>
@@ -18,10 +19,20 @@ const Navbar = () => {
     <li className='app__navbar-logo'><a href='#contact'>Contact</a></li>
   </ul>
   <div className='app__navbar-smallscreen'>
-    <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => {}}/>
-    <div className='app__bavbar-smallscreen_overlay flex_center slide-bottom'>
-      <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => {}} />
+    <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => setToggleMenu(true)}/>
+
+    {toggleMenu && (
+    <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+      <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
+      <ul className='app__navbar-smallscreen_links'>
+    <li><a href='#home'>Home</a></li>
+    <li><a href='#about'>About</a></li>
+    <li><a href='#menu'>Menu</a></li>
+    <li><a href='#contact'>Contact</a></li>
+  </ul>
     </div>
+    )} 
+
   </div>
 </nav>
   )
