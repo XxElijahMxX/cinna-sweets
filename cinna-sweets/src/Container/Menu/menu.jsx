69c4a065cd03b3021428c9d1ac94e5f38f718b95
Sheridan Melton <99241310+XxElijahMxX@users.noreply.github.com>
2023-02-05 12:21:07 -0500

    started working on menu component

--- cinna-sweets/src/Container/Menu/menu.jsx
+++ cinna-sweets/src/Container/Menu/menu.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import './menu.css'
+import { SubHeader, MenuItem } from '../../Components'
+import { images, data } from '../../constants'
 
-const menu = () => {
-  return (
-    <div>menu</div>
+const menu = () => (
+    <div className='app__menu flex__center section__padding' id='menu'>
+    <div className='app__menu-title'>
+      <SubHeader title='Sweets To Die For' />
+      <h1 className='headtext__cormorant'>Cinna's Specials</h1>
+    </div>
+
+        <div className='app__menu-specials'>
+          <div className='app__menu-specials-pastries flex__center'>
+            <p className='app__menu-specials-heading'>Pastries</p>
+            <div className='app__menu-special-menu_items'></div>
+          </div>
+        </div>
+      </div>
   )
-}
+
 
 export default menu
